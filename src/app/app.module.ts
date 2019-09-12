16a0c05920370f66fffba7a051ff689a745a46a1
Roberto Jose De Lavor Rodrigues <r.de.lavor.rodrigues@avanade.com>
2019-09-12 10:39:32 -0300

    Updating repository with all files

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent
+  ],
+
+  // providers: [],
+  bootstrap: [EventsAppComponent]
+})
+export class AppModule { }
