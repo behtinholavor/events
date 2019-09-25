2af1a94d7215a80739880c04c96d3ba672749da4
Roberto Jose De Lavor Rodrigues <r.de.lavor.rodrigues@avanade.com>
2019-09-25 12:51:03 -0300

    Inlcuded id on routes to navigate on the event list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
@@ -8,9 +9,12 @@ import { NavBarComponent } from './nav/nav-bar.component';
 import { EventService } from './events/shared/event.service'
 import { ToastrService } from './common/toastr.service';
 import { EventDetailsComponent } from "./events/event-details/event-details.component";
+import { appRoutes } from "../routes";
+
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     NavBarComponent,
