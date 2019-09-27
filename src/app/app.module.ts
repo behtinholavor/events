16518eeafaf01b78f1c0c6bcc0c3dfeaea200c82
Roberto Jose De Lavor Rodrigues <r.de.lavor.rodrigues@avanade.com>
2019-09-27 13:15:45 -0300

    Create barrels file (index.ts) to encapsulated imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
-import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
-import { NavBarComponent } from './nav/nav-bar.component';
-import { EventService } from './events/shared/event.service'
-import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from "./events/event-details/event-details.component";
 import { appRoutes } from "../routes";
-import { CreateEventComponent } from "./events/create-event.component";
+
+import {
+  CreateEventComponent,
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventDetailsComponent,
+  EventService,
+  EventListResolver,
+  EventRouteActivator
+} from './events/index';
+
+import { NavBarComponent } from './nav/nav-bar.component';
+import { EventsAppComponent } from './events-app.component';
+import { ToastrService } from './common/toastr.service';
 import { Error404Component } from './erros/404.component';
-import { EventRouteActivator } from "./events/event-details/event-route-activator.service";
-import { EventListResolver } from "./events/events-list-resolver.service";
 @NgModule({
   imports: [
     BrowserModule,
