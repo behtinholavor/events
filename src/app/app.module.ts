ba073a147f41b65ff3b30729b6f238a782d5cba9
Roberto Jose De Lavor Rodrigues <r.de.lavor.rodrigues@avanade.com>
2019-09-27 09:48:44 -0300

    Implemented validation fmro routes, Erro 404, Activator route and adding use feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,10 @@ import { EventService } from './events/shared/event.service'
 import { ToastrService } from './common/toastr.service';
 import { EventDetailsComponent } from "./events/event-details/event-details.component";
 import { appRoutes } from "../routes";
-
+import { CreateEventComponent } from "./events/shared/create-event.component";
+import { Error404Component } from './erros/404.component';
+import { EventRouteActivator } from "./events/event-details/event-route-activator.service";
+import { EventListResolver } from "./events/events-list-resolver.service";
 @NgModule({
   imports: [
     BrowserModule,
@@ -21,9 +24,22 @@ import { appRoutes } from "../routes";
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
-    EventDetailsComponent
+    EventDetailsComponent,
+    CreateEventComponent,
+    Error404Component
   ],
-  providers: [EventService, ToastrService],
+  providers: [
+    EventService,
+    ToastrService,
+    EventRouteActivator,
+    EventListResolver,
+    { provide: 'canDeactivateCreateEvent', useValue: checkDirtyState }
+  ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component: CreateEventComponent) {
+  if (component.isDirty)
+    return window.confirm('You have not saved this even. do you really qant to cancel?');
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from "@angular/router";
 import { EventsListComponent } from './app/events/events-list.component';
 import { EventDetailsComponent } from './app/events/event-details/event-details.component';
+import { CreateEventComponent } from './app/events/shared/create-event.component';
+import { Error404Component } from './app/erros/404.component';
+import { EventRouteActivator } from './app/events/event-details/event-route-activator.service';
+import { EventListResolver } from './app/events/events-list-resolver.service';
 
 export const appRoutes: Routes = [
-    { path: 'events', component: EventsListComponent },
-    { path: 'events/:id', component: EventDetailsComponent },
+    { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
+    { path: 'events', component: EventsListComponent, resolve: { events: EventListResolver } },
+    { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
+    { path: '404', component: Error404Component },
     { path: '', redirectTo: '/events', pathMatch: 'full' },
+    { path: 'user', loadChildren: './app/events/user/user.module#UserModule' },
 ]
